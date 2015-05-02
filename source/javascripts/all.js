d7b9bfb4533e6ddefc36723d1c61de2c48d2f0c3
adi-cam <adina.renner@gmail.com>
2015-05-02 12:37:38 +0200

    more d3 magic

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -11,6 +11,25 @@ function updateList(){
   items.exit().remove();
 }
 
+function updateGraph(){
+  var items = d3.select('.graph').selectAll('.point').data(DATA);
+
+  items.enter().append('circle')
+    .attr('class', 'point')
+    .attr('r', function(d, i){
+      console.log(this, d, i);
+
+      return d.year - 1920;
+    })
+    .attr('cx', function(_, i){
+      return i * 300 + 10;
+    })
+    .attr('cy', 200);
+
+  items.exit().remove();
+}
+
 $(function(){
   updateList();
+  updateGraph();
 });
