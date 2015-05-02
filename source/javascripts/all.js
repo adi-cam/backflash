0c25a2dbca61846007c89bb3b065bb8e1ef56818
adi-cam <adina.renner@gmail.com>
2015-05-02 12:17:19 +0200

    added some d3 magic

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,16 @@
 //= require './data'
 //= require_self
+
+function updateList(){
+  var items = d3.select('.list').selectAll('.movie').data(DATA);
+
+  items.enter().append('div')
+    .attr('class', 'movie')
+    .text(function(d) { return d.title; });
+
+  items.exit().remove();
+}
+
+$(function(){
+  updateList();
+});
