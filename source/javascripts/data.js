e846ed9d4b4269faf83063ece82077a41fedea82
adi-cam <adina.renner@gmail.com>
2015-05-02 11:59:58 +0200

    Dataset

---
+++ source/javascripts/data.js
@@ -0,0 +1,39 @@
+var GENRES = {
+  1: 'horror',
+  2: 'action',
+  3: 'drama',
+  4: 'sci-fi'
+};
+
+var DATA = [
+  {
+    title: 'Forbidden Planet',
+    year: 1989,
+    date: new Date(2015, 5, 2),
+    genre: 1
+  },
+  {
+    title: 'The Avengers',
+    year: 2015,
+    date: new Date(2015, 4, 28),
+    genre: 2
+  },
+  {
+    title: "L'atalantet",
+    year: 1934,
+    date: new Date(2015, 4, 5),
+    genre: 3
+  },
+  {
+    title: 'Contact',
+    year: 1997,
+    date: new Date(2015, 3, 25),
+    genre: 4
+  },
+  {
+    title: 'District 9',
+    year: 2009,
+    date: new Date(2015, 3, 10),
+    genre: 4
+  }
+];
