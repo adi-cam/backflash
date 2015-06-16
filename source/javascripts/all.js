1c9738eafd8c89b1e976dd48c58f874c36e5dd19
adinarenner <adina@interactivethings.com>
2015-06-16 11:11:05 +0200

    First Request to OMDB API

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,46 +1,18 @@
-//= require './data'
 //= require_self
 
-d3.csv("/data/Dataset_Veranstaltunge.csv",function(error,data) {
-    dataViz(data);
-}});
 
-function dataViz(incomingData) {
-    var maxPopulation = d3.max(incomingData, function (el) {
-            return parseInt(el.population);
-        }
-    );
-}
+var Request = new XMLHttpRequest();
 
-function updateList(){
-  var items = d3.select('.list').selectAll('.movie').data(data);
+Request.open('GET', 'http://www.omdbapi.com/?t=Asphalt&y=1929&plot=short&r=json');
 
-  items.enter().append('div')
-    .attr('class', 'movie')
-    .text(function(d) { return d.title; });
+Request.setRequestHeader('Accept', 'application/json');
 
-  items.exit().remove();
-}
+Request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+        console.log('Status:', this.status);
+        console.log('Headers:', this.getAllResponseHeaders());
+        console.log('Body:', this.responseText);
+    }
+};
 
-function updateGraph(){
-  var items = d3.select('.graph').selectAll('.point').data(DATA);
-
-  items.enter().append('circle')
-    .attr('class', 'point')
-    .attr('r', function(d, i){
-      console.log(this, d, i);
-
-      return d.year - 1920;
-    })
-    .attr('cx', function(_, i){
-      return i * 300 + 10;
-    })
-    .attr('cy', 200)
-
-  items.exit().remove();
-}
-
-$(function(){
-  updateList();
-  updateGraph();
-});
+Request.send(); 
