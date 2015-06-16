563384e307ab40b1d5f26dcf3613c63f762ef11c
adinarenner <adina@interactivethings.com>
2015-06-16 19:58:16 +0200

    working data enrichment

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,30 @@
 //= require_self
 
-d3.csv("data/films.csv",function(error,data) {
-    dataViz(data);
+d3.csv("data/films.csv",function(error, data) {
+    getInfo(data);
 });
 
+function getInfo(incomingData) {
+    var c = 0;
+    for (var i = 0; i < incomingData.length; i++) {
+        var movie = incomingData[i];
+        axios.get('http://www.omdbapi.com/', {
+            params: {
+                t: movie.Film
+            }
+        }).then(function(response) {
+            movie.runtime = response.data.Runtime;
+            c++;
+            if(c == incomingData.length) {
+                dataViz(incomingData);
+            }
+        });
+    };
 
+};
 
 function dataViz(incomingData) {
+    console.log('go d3!');
 /*
     d3.select("body").selectAll("div.films")
         .data(incomingData)
@@ -17,27 +35,4 @@ function dataViz(incomingData) {
             return d.Film;
         });
 */
-
-    var movieArray = [];
-    incomingData.map(function(d){
-        movieArray.push(d.Film)
-    });
-
-
-    function getInfo() {
-        for (var i = 0; i < movieArray.length; i++) {
-            axios.get('http://www.omdbapi.com/', {
-                params: {
-                    t: movieArray[i]
-                }
-            }).then(function(response) {
-                var dataAPI = response.data;
-                var dataAPIRuntime  = dataAPI.Runtime;
-                console.log(dataAPIRuntime);
-            });
-        };
-
-    };
-
-    getInfo();
 }
