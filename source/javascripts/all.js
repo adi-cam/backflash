ab392cb9310abc6155195118a2b3259477f452b7
adinarenner <adina@interactivethings.com>
2015-06-16 23:49:06 +0200

    DONE!

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,19 +1,21 @@
 //= require_self
 
 d3.json('data.json', function(_, data) {
-    dataViz(data);
+  dataViz(data);
 });
 
 function dataViz(data) {
-    console.log('go d3!', data);
-/*
-    d3.select("body").selectAll("div.films")
-        .data(data)
-        .enter()
-        .append("div")
-        .attr("class", "films")
-        .html(function (d, i) {
-            return d.Film;
-        });
-*/
+  var items = d3.select('.graph').selectAll('.point').data(data.movies);
+
+  items.enter().append('circle')
+    .attr('class', 'point')
+    .attr('r', function(d){
+      return d.length / 10;
+    })
+    .attr('cx', function(_, i){
+      return i * 30 + 10;
+    })
+    .attr('cy', 200);
+
+  items.exit().remove();
 }
