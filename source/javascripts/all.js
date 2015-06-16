75b6c98be54b89b2e0db687f0b46ee0c93819e73
adinarenner <adina@interactivethings.com>
2015-06-16 13:32:47 +0200

    Installed Axios and tried it for the first time

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,18 +1,10 @@
 //= require_self
 
-
-var Request = new XMLHttpRequest();
-
-Request.open('GET', 'http://www.omdbapi.com/?t=Asphalt&y=1929&plot=short&r=json');
-
-Request.setRequestHeader('Accept', 'application/json');
-
-Request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-        console.log('Status:', this.status);
-        console.log('Headers:', this.getAllResponseHeaders());
-        console.log('Body:', this.responseText);
+axios.get('http://www.omdbapi.com/', {
+    params: {
+        s: 'Frozen',
+        y:'true'
     }
-};
-
-Request.send(); 
+}).then(function(response){
+    console.log(response.data)
+})
