b51833e39296d279001816efa8f0b2a86d0e9cd0
adinarenner <adina@interactivethings.com>
2015-06-20 11:10:21 +0200

    cleanup

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -77,33 +77,29 @@ function dataViz(data) {
 // –––––––––––––––––––––––––––––––––––––
 
   //create location scales
-  var maxRuntime = d3.max(movies, function (m) {
-    return m.length
-  });
+  var maxRuntime = d3.max(movies, function(m){ return m.length });
   var radiusScale = d3.scale.sqrt().domain([0, maxRuntime]).range([0, 50]);
 
   var yScaleRange = d3.scale.ordinal().domain(uniqueYKeys).rangeRoundPoints([0, uniqueYKeys.length-1]);
   var yScale = d3.scale.linear().domain([0, uniqueYKeys.length-1]).range([0, 900]);
 
   //create array for months (9x0)
-  var xCursors = d3.range(uniqueYKeys.length).map(function(){ return 1; });
+  var xPositions = d3.range(uniqueYKeys.length).map(function(){ return 1; });
 
   // create color scale
   var colorScaleGenre = d3.scale.category20([uniqueGenres]);
   var colorScaleCountry = d3.scale.category20([movies._region]);
 
-  //get min and max from year
-  var extentYear = d3.extent(movies, function (m) {
-    return m.year;
-  });
+  // get min and max from year
+  var extentYear = d3.extent(movies, function(m){ return m.year; });
 
-  //create opacity scale
+  // create opacity scale
   var opacityScale = d3.scale.linear().domain(extentYear).range([0, 1]);
-  //console.log(extentYear);
-  //create blur scale
+
+  // create blur scale
   var yearScale = d3.scale.linear().domain(extentYear).range([15, 0]);
 
-  //prepare the tooltip div
+  // prepare the tooltip div
   var div = d3.select("body")
       .append("div")
       .attr("class", "tooltip")
@@ -114,81 +110,81 @@ function dataViz(data) {
 // –––––––––––––––––––––––––––––––––––––
 
   var items = d3.select('svg')
-      .selectAll('g')
-      .data(movies, function (m) {
-        return m.id;
-      })
-      .enter()
-      .append('g')
-      .attr('data-id', function (d) {
-        return d.id;
-      })
-      .attr("transform", function (d, i) {
-        var m2 = yScaleRange(d._event._yKey);
-        var x = xCursors[m2];
-        var r = radiusScale(d.length);
-        xCursors[m2] += r * 2 + 20;
-        return "translate (" + ((x + r) + 50) + "," + ((yScale(m2)) + 50) + ")";
-      });
+    .selectAll('g')
+    .data(movies, function (m) {
+      return m.id;
+    })
+    .enter()
+    .append('g')
+    .attr('data-id', function (d) {
+      return d.id;
+    })
+    .attr("transform", function (d, i) {
+      var m2 = yScaleRange(d._event._yKey);
+      var x = xPositions[m2];
+      var r = radiusScale(d.length);
+      xPositions[m2] += r * 2 + 20;
+      return "translate (" + ((x + r) + 50) + "," + ((yScale(m2)) + 50) + ")";
+    });
 
 
   //Blur Filter
   var filter = items.append("defs")
-      .append("filter")
-      .attr("id", "feGaussianBlur")
-      .attr('x', "-200%")
-      .attr('y', "-200%")
-      .attr('width', "500%")
-      .attr('height', "500%")
-      .append("feGaussianBlur")
-      .attr("stdDeviation", function (d) {
-        return yearScale(d.year);
-      })
-      .attr("result", "blur");
+    .append("filter")
+    .attr("id", "feGaussianBlur")
+    .attr('x', "-200%")
+    .attr('y', "-200%")
+    .attr('width', "500%")
+    .attr('height', "500%")
+    .append("feGaussianBlur")
+    .attr("stdDeviation", function (d) {
+      return yearScale(d.year);
+    })
+    .attr("result", "blur");
 
   var feMerge = filter.append("feMerge");
 
   feMerge.append("feMergeNode")
-      .attr("in", "blur")
+    .attr("in", "blur");
   feMerge.append("feMergeNode")
-      .attr("in", "SourceGraphic");
+    .attr("in", "SourceGraphic");
 
 
   items.append('circle')
-      .attr('class', 'movie')
-      .attr('r', function (d) {
-        return radiusScale(d.length)
-      })
+    .attr('class', 'movie')
+    .attr('r', function (d) {
+      return radiusScale(d.length)
+    })
     //.style('fill', function (d) {return colorScaleGenre(d.genre)})
-      .style('fill', function (d) {
-        return colorScaleCountry(d._region)
-      })
+    .style('fill', function (d) {
+      return colorScaleCountry(d._region)
+    })
     //.style('fill-opacity', function (d) {return opacityScale(d.year)})
-      .style("filter", "url(#feGaussianBlur)")
-      .on("mouseover", function (d) {
-        div.transition()
-            .duration(500)
-            .style("opacity", 0);
-        div.transition()
-            .duration(200)
-            .style("opacity", .9);
-        div.html(d.title + ' ' + d._region + ' ' + d.genre + ' ' + d.year + ' ' + d._event._date)
-            .style("left", (d3.event.pageX - 20) + "px")
-            .style("top", (d3.event.pageY - 40) + "px");
-      });
+    .style("filter", "url(#feGaussianBlur)")
+    .on("mouseover", function (d) {
+      div.transition()
+        .duration(500)
+        .style("opacity", 0);
+      div.transition()
+        .duration(200)
+        .style("opacity", .9);
+      div.html(d.title + ' ' + d._region + ' ' + d.genre + ' ' + d.year + ' ' + d._event._date)
+        .style("left", (d3.event.pageX - 20) + "px")
+        .style("top", (d3.event.pageY - 40) + "px");
+    });
   var text = d3.select('body')
-      .selectAll('p')
-      .data(movies)
-      .enter()
-      .append('p')
-      .attr('class', 'months')
-      .html(function (d){
-        return d._event._date.toString().split(' ')[1];
-      })
-      .attr('x', 10)
-      .attr('y', function (d) {
-        return yScale(yScaleRange(d._event._date.getMonth()));
-      });
+    .selectAll('p')
+    .data(movies)
+    .enter()
+    .append('p')
+    .attr('class', 'months')
+    .html(function (d){
+      return d._event._date.toString().split(' ')[1];
+    })
+    .attr('x', 10)
+    .attr('y', function (d) {
+      return yScale(yScaleRange(d._event._date.getMonth()));
+    });
 }
 
 //TODO: put movies in right order in month
